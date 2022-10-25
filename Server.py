d193685316c328dad4527d60db0fde1fbc7c0efd
“csureshkumargit” <csureshkumargce@gmail.com>
2022-10-25 01:31:24 -0400

    Added Code Repo

--- Server.py
+++ Server.py
@@ -124,5 +124,5 @@ def updateUser() :
 
 ### Service Listener ###
 if __name__ == "__main__":
-    app.run(host="localhost", port=8090, debug=True)
+    app.run(port=8090, debug=True)
 #########################
